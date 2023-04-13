773c9c03f19f8e37c1428c404162a35edee46ceb
Fares <Fares@192.168.1.16>
2023-04-13 07:16:40 +0200

    number to string implementation

--- Bubble_Sorting.c
+++ Bubble_Sorting.c
@@ -2,7 +2,7 @@
 /* implementation of Bubble sorting*/
 int main()
 {
-    int arr[10]= {0};
+   /* int arr[10]= {0};
     int i,temp;
     for(i=0; i<10; i++)
     {
@@ -28,4 +28,36 @@ int main()
     {
         printf("%d\n",arr[z]);
     }
+*/
+int arr[10],temp;
+for (int i = 0; i < 10; i++)
+{
+printf("please enter the %d number:\n",i+1);
+scanf("%d",&arr[i]);
+}
+
+for (int i = 0; i < 9; i++)
+{
+
+    for (int z = 0; z < 9-i ; z++)
+    {
+        if(arr[z]>arr[z+1])
+        {
+            temp=arr[z+1];
+            arr[z+1]=arr[z];
+            arr[z]=temp;
+        }
+    }
+    
+
+}
+
+
+for (int i = 0; i <= 9; i++)
+{
+printf("array after sorting is%d\n",arr[i]);
+
+}
+
+
 }
